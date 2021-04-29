6752697ac8c82b1ec6fbdb3f1c7c3dd9781a0c15
shajiran <78599931+shajiran@users.noreply.github.com>
2021-04-29 19:02:39 +0200

    Index.php: Codes ergänzt

--- lb3/index.php
+++ lb3/index.php
@@ -1,1 +1,13 @@
+<?php
+$host = 'mysql';
+$user = 'Raveendran';
+$password = 'Raveendran123';
+$db = 'Raveendran_db';
 
+$connection = new mysqli($host,$user,$password,$db);
+if($connection->connect_error){
+    echo 'Connection FAILED' . $connection->connect_error;
+}
+echo 'Raveendran Shajiran MYSQL: Connected';
+
+?>
